fce87d5b23b9e6a91454f5d05748e3d3238955c0
YOGANANDH60 <yoga132005@gmail.com>
2025-01-06 23:31:27 +0530

    header create

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 968,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 880,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
